4346eb36c9d373345612917d6c94fbb5b43cfcfc
NoelBird <lduldu00228@gmail.com>
2021-05-23 09:03:23 +0000

    update contents

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 const express = require('express'); // express 패키지 불러오기
+const https = require('https');
+const fs = require('fs');
+const http = require('http');
 var app = express(); // express로 앱 생성
 
 app.use('/static', express.static('public')); // static 폴더를 /public directory로 지정하기
@@ -7,6 +10,11 @@ app.set('views', __dirname + '/views'); // view 폴더를 /views로 지정하기
 app.set('view engine', 'ejs'); // 렌더링 엔진은 ejs
 
 app.engine('html', require('ejs').renderFile); // html 파일을 ejs로 렌더링하기
+
+const options = {
+  key: fs.readFileSync('keys/key.pem'),
+  cert: fs.readFileSync('keys/key-cert.pem')
+}
 
 const IP="0.0.0.0"; // service ip //TODO: 디버그 모드와 릴리즈 모드로 나누기
 const PORT=80; // service port
@@ -29,4 +37,5 @@ app.get('/base', (req, res) => {
     res.render("base")
 });
 
-app.listen(PORT, IP);
+http.createServer(app).listen(PORT);
+https.createServer(options, app).listen(443);
